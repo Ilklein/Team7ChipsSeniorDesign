d8d95eeb93d4b45a205e80c8ce552b7f5a07e53e
Isaac <isaacgray52@gmail.com>
2025-09-06 15:09:55 -0400

    added counter module

---
+++ src/counter.v
@@ -0,0 +1,23 @@
+module counter #(
+    parameter MAX = 143
+    parameter WIDTH = 8
+) (
+    input clk,
+    input en,
+    output [WIDTH,0] count
+);
+    
+    always @(posedge clk) begin
+
+        
+        if(en & count != MAX) begin 
+            count = count+1;
+        end 
+        else begin
+            count = 0;
+        end
+
+    end
+
+
+endmodule
