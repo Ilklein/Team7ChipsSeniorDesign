e097db90024714bad10e176febfac9cb167cb4d6
isaacgray52 <65960595+isaacgray52@users.noreply.github.com>
2026-02-03 12:36:09 -0500

    Update SIPO9.v
    Updated SIPO9 with my latest version

--- src/SIPO9.v
+++ src/SIPO9.v
@@ -1,3 +1,4 @@
+
 module SIPO9 (
     input wire clk,
     input wire rst,
@@ -15,12 +16,13 @@ always @(posedge clk) begin
         out <= 16'b0;
         count <= 0;
         sipo_done <= 0;
-        busy <= 0;
+        busy <= 1;
     end
     else begin
         if (!busy) begin
-            out <= {out[14:0], in};
+            out <= {in, out[15:1]};
             count <= count + 1;
+            
             if(count == 4'd15) begin
                 sipo_done <= 1;
                 busy <= 1;         // prevent counting again
@@ -29,8 +31,9 @@ always @(posedge clk) begin
         else begin
             sipo_done <= 0;
             if(valid_data) begin // once start is triggered, start new triangle
-                count <= 0;
+                count <= 1;
                 busy <= 0;
+                out <= {in, out[15:1]};
             end
         end
     end
